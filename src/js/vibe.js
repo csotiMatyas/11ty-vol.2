7b98044445c6d3b36848aaa36ba10b5f4d9cca68
Csoti Matyas <csotimatyas93@gmail.com>
2021-11-11 17:30:55 +0200

    Initial commit

---
+++ src/js/vibe.js
@@ -0,0 +1,4 @@
+const vibes = ["....asdasd","...asdasdd","dadqdqdadad","doeogqgq","..agghihihio"];
+const vibe = vibes[Math.floor(Math.random()*Math.floor(vibes.length))];
+
+document.querySelector(".vibe").append(vibe);
