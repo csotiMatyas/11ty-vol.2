66f1dc71255463a53c79464c11b1fa2545a64e46
Csoti Matyas <csotimatyas93@gmail.com>
2021-11-15 10:43:38 +0200

    localized content

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,12 +1,21 @@
 const axios = require("axios");
 require("dotenv").config();
+const countries = require("./countries.json");
 
-module.exports = async function() {
+const getNews = async(country) => {
   try{
-    const { data } = await axios.get(`https://newsapi.org/v2/top-headlines?country=ro&apiKey=${process.env.API_KEY}`)
-    return data
+    const { data } = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.API_KEY}`)
+    return {
+      country,
+      articles: data.articles 
+    }
   }
   catch(err){
-    console.log(err)
+    console.log("ERROR>>>", err.response)
   }
+};
+
+module.exports = async function() {
+  const newsPromise = countries.map(getNews);
+  return await Promise.all(newsPromise);
 }
--- src/js/vibe.js
+++ src/js/vibe.js
@@ -2,3 +2,4 @@ const vibes = ["....asdasd","...asdasdd","dadqdqdadad","doeogqgq","..agghihihio"
 const vibe = vibes[Math.floor(Math.random()*Math.floor(vibes.length))];
 
 document.querySelector(".vibe").append(vibe);
+
